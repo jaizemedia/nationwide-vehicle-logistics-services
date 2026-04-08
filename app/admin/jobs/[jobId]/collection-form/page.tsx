55455c0495871f34640ddc72476d617376b9f09b
jaizemedia <21armah-godwinj@watling.academy>
2026-04-08 14:06:43 +0000

    ddd

--- app/admin/jobs/[jobId]/collection-form/page.tsx
+++ app/admin/jobs/[jobId]/collection-form/page.tsx
@@ -1,4 +1,9 @@
 "use client";
-import AdminCollectionForm from '../collection-form';
+import { useParams } from 'next/navigation';
+import { CollectionFormView } from '@/components/CollectionFormView';
 
-export default AdminCollectionForm;
+export default function AdminCollectionFormPage() {
+	const params = useParams();
+	const jobId = params.jobId as string;
+	return <CollectionFormView jobId={jobId} readOnly />;
+}
